162f3d28c2b9a1bfb1f74c32338564c45769e03e
Copying <martiangelats@hotmail.com>
2015-09-02 00:29:01 +0200

    Removed the items. Continue with tests

--- js/data.js
+++ js/data.js
@@ -1,15 +1,18 @@
-function getItems(filename, element)
+var xmlDoc;
+function olh(e)
 {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "./data/" + filename, true);
-	xhr.onload = function (e) {
 	if (xhr.readyState === 4) {
 		if (xhr.status === 200) {
-			console.log(xhr.responseText);
+			console.log(xmlDoc = xhr.responseText);
 		} else {
 			console.error(xhr.statusText);
 		}
 	}
-};
+}
+function getItems(filename, element)
+{
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "./data/" + filename, true);
+	xhr.onload = olh;
 	xhr.send();
 }
