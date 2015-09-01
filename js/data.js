64ef00b17d088ca346badd00e35ae7365b8c9fed
Copying <martiangelats@hotmail.com>
2015-09-02 00:25:28 +0200

    no message

--- js/data.js
+++ js/data.js
@@ -1,7 +1,15 @@
 function getItems(filename, element)
 {
-	var request = new XMLHttpRequest();
-	request.open("GET", "./data/" + filename, false);
-	request.send();
-	return request.responseXML;
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "./data/" + filename, true);
+	xhr.onload = function (e) {
+	if (xhr.readyState === 4) {
+		if (xhr.status === 200) {
+			console.log(xhr.responseText);
+		} else {
+			console.error(xhr.statusText);
+		}
+	}
+};
+	xhr.send();
 }
