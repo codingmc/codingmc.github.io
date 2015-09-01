dbf97f14e7c126ec3b320d75cddf06f91e5660dd
Copying <martiangelats@hotmail.com>
2015-09-02 00:53:50 +0200

    Now adds elements

--- js/data.js
+++ js/data.js
@@ -2,20 +2,44 @@
 
 var xhr = null;
 var xmlDoc = null;
-function olh(e)
+function setItems(xmlDoc, element)
 {
-	if (xhr.readyState === 4) {
-		if (xhr.status === 200) {
-			console.log(xmlDoc = xhr.responseXML);
-		} else {
-			console.error(xhr.statusText);
+	var item = xmlDoc.getElementById("Command").firstElementChild;
+
+	while (item)
+	{
+		var div = document.createElement("div");
+		div.innerHTML = "<p>" + item.getAttribute("ref") + "</p>";
+
+		var v = item.getAttribute("output");
+		if (v == "-1")
+		{
+			div.setAttribute("data-friendly", "");
 		}
+		else if (parseInt(v) >= 3)
+		{
+			div.setAttribute("data-output", "");
+		}
+
+		element.appendChild(div);
+
+		item = item.nextElementSibling;
 	}
 }
 function getItems(filename, element)
 {
 	xhr = new XMLHttpRequest();
 	xhr.open("GET", "./data/" + filename, true);
-	xhr.onload = olh;
+	xhr.onload = function (e)
+	{
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				console.log(xmlDoc = xhr.responseXML);
+				setItems(xmlDoc, element);
+			} else {
+				console.error(xhr.statusText);
+			}
+		}
+	};
 	xhr.send();
 }
