96b774d6b5b1f21e82838a93db6db00847e08413
Copying <martiangelats@hotmail.com>
2015-09-02 00:19:46 +0200

    Help testing

--- js/data.js
+++ js/data.js
@@ -1,9 +1,10 @@
+var xmlDoc;
 function getItems(filename, element)
 {
 	var request = new XMLHttpRequest();
 	request.open("GET", "./data/" + filename, false);
 	request.send();
-	var xmlDoc = request.responseXML;
+	xmlDoc = request.responseXML;
 
 
 }
